d52c4573ce4137749f5d519ae62f0623e7ce7d32
Montse <mtschamorro@gmail.com>
2014-11-12 13:10:33 +0100

    creado mth getDisplayValue, que devuelve el valor actual del display en forma de cadena de 2 caracteres

--- miproyecto/NumberDisplay.java
+++ miproyecto/NumberDisplay.java
@@ -14,19 +14,52 @@ public class NumberDisplay
     /**
      * Constructor for objects of class NumberDisplay
      */
-    public NumberDisplay(int valorMaximo)
+    public NumberDisplay(int limit)
     {
-        límite = valorMaximo;
+        límite = limit;
         valorActual = 0;
 
     }
+    /**
+     * metodo que fija el valor actual del display
+     */
     public void setValue(int newValorActual)
     {
         valorActual = newValorActual;
     }
-    
+    /**
+     * metodo que devuelve el valor actual en forma de cadena de 2 caracteres
+     */
+    public String getDisplayValue()
+    {
         
-
-    
+       int valorAc = valorActual;
+        if (valorAc<10)
+        {
+            return "0" + valorAc;
+        }
+        else
+        {
+            return "" + valorAc;
+        }
+   
+    }
+/**
+ * metodo que devuelve el valor actual del display como entero
+ */
+public int getValue()
+{
+    return valorActual;
+}
+/**
+ * metodo que incrementa en 1 al display y lo hace volver al principio si alcanza el límite
+ */
+public void increment()
+{
+   
+}
+        
+      
+          
     
 }
